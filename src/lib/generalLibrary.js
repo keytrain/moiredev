87354c3fdffccaf6bd274ecc9e0fb5a77ac19788
keytrain <keytrain@users.noreply.github.com>
2017-09-04 12:34:44 -0700

    Add - Hour, Minute, and Second support for release time

--- src/lib/generalLibrary.js
+++ src/lib/generalLibrary.js
@@ -4,33 +4,59 @@ export default {
     tYear = today.getFullYear(),
     tMonth = today.getMonth() + 1,
     tDay = today.getDate() + 1,
-    dYear = dateObj.getFullYear(),
-    dMonth = dateObj.getMonth() + 1,
-    dDay = dateObj.getDate() + 1;
+    tHour = today.getHours(),
+    tMin = today.getMinutes(),
+    tSec = today.getSeconds(),
+    rYear = dateObj.getFullYear(),
+    rMonth = dateObj.getMonth() + 1,
+    rDay = dateObj.getDate() + 1,
+    rHour = dateObj.getHours(),
+    rMin = dateObj.getMinutes(),
+    rSec = dateObj.getSeconds();
+    
+    let yDiff = tYear - rYear;
+    let moDiff = tMonth - rMonth;
+    let dDiff = tDay - rDay;
+    let hDiff = tHour - rHour;
+    let mDiff = tMin - rMin;
+    let sDiff = tSec - rSec;
 
-    let yDiff = tYear - dYear;
-    let mDiff = tMonth - dMonth;
-    let dDiff = tDay - dDay;
+    // if (rDay === 5) {
+    //   console.log(dateObj);
+    //   console.log('thour:' + tMin + ' rMin:' + rMin);
+    // }
 
     if (yDiff > 0) {
       // different year but within 12 months
-      if (yDiff === 1 && mDiff < 0) {
-        return (mDiff + 12) + 'mo';
-      }
-      else {
+      if (yDiff === 1 && moDiff < 0) {
+        return (moDiff + 12) + 'mo';
+      } else {
         return yDiff + 'y';
       }
+    } else if (moDiff > 0) {
+      // different month but within 30 days
+      if (moDiff === 1 && dDiff < 0) {
+        return (dDiff + 30) + 'd';
+      }
+      return moDiff + 'mo';
+    } else if (dDiff > 0) {
+      if (dDiff === 1 && hDiff < 0) {
+        return (hDiff + 24) + 'h';
+      }
+      return dDiff + 'd';
+    } else if (hDiff > 0) {
+      if (hDiff === 1 && mDiff < 0) {
+        return (mDiff + 60) + 'm';
+      }
+      return hDiff + 'h';
+    } else if (mDiff > 0) {
+      if (mDiff === 1 && sDiff < 0) {
+        return (sDiff + 60) + 's';
+      }
+      return mDiff + 'm';
+    } else if (sDiff > 0) {
+      return sDiff + 's';
     }
-    else if (mDiff > 0) {
-      return mDiff + 'mo';      
-    }
-    else if (dDiff > 0) {
-      return dDiff + 'd';      
-    }
-    else {
-
-    }
-
     return 'Error';
   },
 
